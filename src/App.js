9a46e1401395428cf119e48a81771074b6fc2014
rururu16 <ynk618172@gmail.com>
2018-11-07 15:10:33 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    // const greeting = "Hi!"
-    // const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
-    // return <input type="text" onChange={()=>{console.log("I am Clicked!")}} />
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        < input type = "text"
-        onChange = {
-          () => {
-            console.log("I am Clicked!")
-          }
-        }
-        />
-      </React.Fragment>
-    )  
-  }
+// //  Appはクラスコンポーネント
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi!"
+//     // const dom = <h1 className="foo">{greeting}</h1>;
+//     // return dom;
+//     // return <input type="text" onChange={()=>{console.log("I am Clicked!")}} />
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         < input type = "text"
+//         onChange = {
+//           () => {
+//             console.log("I am Clicked!")
+//           }
+//         }
+//         />
+//       </React.Fragment>
+//     )  
+//   }
+// }
+
+// 関数コンポーネント、ファンクションコンポーネント
+const App = () =>{
+  return (
+  <div>
+    <Cat />
+    <Cat />
+  </div>
+  )
 }
-
+const Cat = () => {
+  return (
+  <div>Meow!</div>
+  )
+}
 export default App;
