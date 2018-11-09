3fd2c6c06147b8aec24f1e5aa922eb1f98ee7a2d
rururu16 <ynk618172@gmail.com>
2018-11-09 10:07:12 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-// stateは変更可能：ミュータブル
+import { increment, decrement } from '../actions'
 
-const App = () =>( <Counter></Counter>)
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = {count: 0}
-  }
-
-  handlePlusButton = () => {
-    // setstateがrenderをコールバック。これで関連するDOMが自動で再描画される
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
+class App extends Component {
   render(){
+    const props = this.props
     return(
       <React.Fragment>
-        <div>count: { this.state.count } </div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value } </div>
+        <button onClick={ props.increment }>+1</button>
+        <button onClick={ props.decrement }>-1</button>
       </React.Fragment >
-
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+// const mapDispatchToProps = dispatch =>({
+//   increment: ()=> dispatch(increment()),
+//   decrement: ()=> dispatch(decrement())
+// })
+// 同じ動作
+const mapDispatchToProps = ({ increment, decrement })
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
+//export default App;
